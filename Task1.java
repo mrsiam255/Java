f7a11111d453049439699e14bc2f30be872f0cae
Ferdoes Siam <122333286+mrsiam255@users.noreply.github.com>
2024-07-15 19:18:20 +0600

    Task1.java

--- Task1.java
+++ Task1.java
@@ -1,6 +1,6 @@
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("My name is Rifat Zaman");
-        System.out.println("My Id:22234103263");
+        System.out.println("My name is Ferdoes Siam");
+        System.out.println("My Id:22234103255");
     }
 }
